d6dc19f0c3795523faf15226c7acd778c2dd5899
Anas Zineddine <azineddi@e1r6p10.1337.ma>
2021-11-02 12:02:43 +0100

    google auth fixed, still need to fix 42auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,20 +1,15 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import FortyTwoProvider from "next-auth/providers/42";
-import GoogleProvider from "next-auth/providers/google";
-
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import FortyTwoProvider from "next-auth/providers/42";
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export default NextAuth({
-  adapter: PrismaAdapter({ prisma }),
+  adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
     FortyTwoProvider({
       clientId: process.env.FORTY_TWO_CLIENT_ID,
       clientSecret: process.env.FORTY_TWO_CLIENT_SECRET,
