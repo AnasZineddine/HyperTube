8fdf6fa5f993a03693195205b47819da1ac5cdfd
Anas Zineddine <azineddi@e1r6p10.1337.ma>
2021-12-09 14:35:24 +0100

    SearchDrawer backup

--- components/SearchButton.js
+++ components/SearchButton.js
@@ -11,10 +11,51 @@ import {
   Input,
   Button,
   Stack,
+  RangeSlider,
+  RangeSliderTrack,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
+  Box,
+  useRadio,
+  useRadioGroup,
+  Center,
+  Grid,
 } from "@chakra-ui/react";
 import React from "react";
 import { Search2Icon } from "@chakra-ui/icons";
 import { useRouter } from "next/router";
+
+function RadioCard(props) {
+  const { getInputProps, getCheckboxProps } = useRadio(props);
+
+  const input = getInputProps();
+  const checkbox = getCheckboxProps();
+
+  return (
+    <Box as="label">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        borderWidth="1px"
+        borderRadius="md"
+        boxShadow="md"
+        _checked={{
+          bg: "blue.300",
+          color: "white",
+          borderColor: "blue.300",
+        }}
+        _focus={{
+          boxShadow: "outline",
+        }}
+        px={5}
+        py={3}
+      >
+        <Center>{props.children}</Center>
+      </Box>
+    </Box>
+  );
+}
 
 const SearchButton = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -23,6 +64,28 @@ const SearchButton = () => {
   const [value, setValue] = React.useState("");
   const handleChange = (event) => setValue(event.target.value);
 
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: "framework",
+    defaultValue: "All",
+    onChange: console.log,
+  });
+
+  const group = getRootProps();
+
+  const options = [
+    "All",
+    "Comedy",
+    "Action",
+    "Horror",
+    "Sci-fi",
+    "Romance",
+    "Thriller",
+    "Drama",
+    "Adventure",
+    "Animation",
+    "Superhero",
+    "Mystery",
+  ];
   return (
     <>
       <IconButton ref={btnRef} onClick={onOpen} icon={<Search2Icon />} />
@@ -52,6 +115,32 @@ const SearchButton = () => {
               >
                 Quick search
               </Button>
+              <Stack pt={10} spacing={10}>
+                <Grid {...group} templateColumns="repeat(2, 1fr)" gap={6}>
+                  {options.map((value) => {
+                    const radio = getRadioProps({ value });
+                    return (
+                      <RadioCard key={value} {...radio}>
+                        {value}
+                      </RadioCard>
+                    );
+                  })}
+                </Grid>
+
+                <RangeSlider
+                  min={1920}
+                  max={2021}
+                  step={5}
+                  defaultValue={[1920, 2021]}
+                  onChangeEnd={(val) => console.log(val)}
+                >
+                  <RangeSliderTrack>
+                    <RangeSliderFilledTrack />
+                  </RangeSliderTrack>
+                  <RangeSliderThumb index={0} />
+                  <RangeSliderThumb index={1} />
+                </RangeSlider>
+              </Stack>
             </Stack>
           </DrawerBody>
           <DrawerFooter>
